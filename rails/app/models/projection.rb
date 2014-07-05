81fb15f1df37f32d8370dda368284db334128db1
landonmarder <landon.marder@gmail.com>
2014-07-05 16:39:17 -0400

    add test for scraping espn site, got to getting name of player

--- rails/app/models/projection.rb
+++ rails/app/models/projection.rb
@@ -1,5 +1,14 @@
+require 'open-uri'
 class Projection < ActiveRecord::Base
 
   belongs_to :player,
     inverse_of: :projections
+
+  def self.scrape_ESPN(url)
+    doc = Nokogiri::HTML(open(url))
+    doc.css('.pncPlayerRow').each do |row|
+      name = row.children[1].children.text.split(',').first
+      binding.pry
+    end
+  end
 end
--- rails/spec/models/projection_spec.rb
+++ rails/spec/models/projection_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 describe Projection do
   it { should belong_to(:player) }
+
+  it 'should scrape ESPN for new projections' do
+    # Making assumption that AP will always be first ranked, will need to change if different :)
+    total_projections = Projection.all.length
+    Projection.scrape_ESPN('http://games.espn.go.com/ffl/tools/projections?&startIndex=0')
+
+    expect(Projection.all.length).to eq(total_projections + 40)
+    expect(Projection.all.first.player.name).to eq('Adrian Peterson')
+  end
 end
