05df21d0456fee284d9d91e967247cc5f195735f
landonmarder <landon.marder@gmail.com>
2014-08-16 14:49:39 -0400

    add ruby scraping to csv for the excel folks

---
+++ to_csv.rb
@@ -0,0 +1,23 @@
+require 'rest_client'
+require 'json'
+require 'pry'
+require 'csv'
+
+response = RestClient.get 'http://fantasy-football-aggregator.herokuapp.com/api/players'
+json = JSON.parse(response)
+
+players = json['players']
+
+file_name = "projections/#{Time.now.strftime('%m-%d-%Y')}-football-projections.csv"
+
+CSV.open(file_name, 'wb') do |row|
+  row << ['Name', 'Team', 'Position', 'Avg. Auction Value', 'Projected Points',
+    'Passing Completions', 'Passing TDs', 'Passing Yards', 'Interceptions', 'Rushing Yards',
+    'Rushing TDs', 'Receiving Yards', 'Receptions', 'Receiving TDs', 'Fumbles']
+  players.each do |player|
+    row << [player['name'], player['team'], player['position'], player['auction_value'],
+    player['projected_points'], player['passing_completions'], player['passing_tds'],
+    player['passing_yards'], player['interceptions'], player['rushing_yards'], player['rushing_tds'],
+    player['receiving_yards'], player['receiving_receptions'], player['receiving_tds'], player['fumbles']]
+  end
+end
